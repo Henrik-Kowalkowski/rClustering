9b6f090d261bf619e7ec489628eb4a36d442fcc9
hkowalkowski <henrikkowalkowski@gmail.com>
2021-11-16 21:42:13 -0600

    Kmeans and pkgdown

--- R/kmeans.R
+++ R/kmeans.R
@@ -1,14 +1,30 @@
-#' Create a 2-d circular Boolean mask
+#' Fit Kmeans on globs
 #'
-#' @param center Cartesian center of the blob specified as c(x, y).
+#' @param data Dataframe of 2-d space to fit on.
+#' @param fit_name Name of kmeans fit
 #' @param n_clust An integer number of clusters to fit.
-#' @param pts Dataframe of Cartesian point coordinates to draw from.
-#' @return Boolean mask when applied forms a circle from a point cloud.
+#' @param iter An integer number of max iterations for the kmeans algorithm to converge to.
+#' @param n_start An integer number of max iterations for the kmeans algorithm to converge to
+#' @param seed Random seed to use to make the results reproducible.
+#' @return Dataframe of covariates, predictions, and title of fit.
 #' @importFrom magrittr "%>%"
 #' @export
-get_mean_clusters <- function(data, n_clust, iter, n_start, clust_order, seed=NULL){
+get_mean_clusters <- function(data, fit_name, n_clust, iter, n_start, seed = NULL) {
   set.seed(seed)
-  fit_data <- data %>% select(x, y) %>% mutate_all(~scale(.) %>% as.vector())
+
+  # Scale input vectors for Kmeans
+  fit_data <- data %>%
+    select(x, y) %>%
+    mutate_all(~ scale(.) %>% as.vector())
 
   start <- Sys.time()
+  # Fit Kmeans and extract cluster labels
+  y_pred <- kmeans(fit_data, n_clust, iter.max = iter, nstart = n_start)$cluster
+  t_time <- round(difftime(Sys.time(), start, units = "secs"), 3)
+  km_data <- cbind(data, y_pred)
+
+  # Create plot titles
+  km_data$title <- glue::glue("{fit_name}, nclust={n_clust}, n_iter={iter}, n_start={n_start}, time={t_time} secs")
+
+  km_data
 }
